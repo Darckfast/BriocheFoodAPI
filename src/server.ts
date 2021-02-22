a3352c6c32bf3ba7a2f42c8294da7d7dbccfe337
Costa <victorcosta37@gmail.com>
2021-02-22 18:09:46 -0300

    chore: adicionado jest e eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-    return res.json({ message: true });
+  return res.json({ message: true })
 })
 
-app.listen(3333); 
+app.listen(3333)
