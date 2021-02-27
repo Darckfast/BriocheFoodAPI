7d9bb69fb19a632860a0fc319914555457f816ba
Costa <victorcosta37@gmail.com>
2021-02-27 14:34:38 -0300

    chore: add rotas

---
+++ src/routes.ts
@@ -0,0 +1,68 @@
+import { UsuarioController } from './controllers/UsuarioController'
+import { AutenticacaoController } from './controllers/AutenticacaoController'
+import { Router } from 'express'
+import { body, oneOf, query } from 'express-validator'
+import { EstabelecimentoController } from '@controllers/EstabelecimentoController'
+import { ProdutoController } from '@controllers/ProdutoController'
+import { cnpj, cpf } from 'cpf-cnpj-validator'
+
+const router = Router()
+
+const usuarioController = new UsuarioController()
+const autenticacaoController = new AutenticacaoController()
+const estabelecimentoController = new EstabelecimentoController()
+const produtoController = new ProdutoController()
+
+router.post(
+  '/api/v1/usuario',
+  body('email').isEmail().not().isEmpty().normalizeEmail(),
+  body('login').isLength({ min: 5, max: 255 }).matches(/^[a-zA-Z0-9.@_]*$/).not().isEmpty().trim(),
+  body('senha').isLength({ min: 8, max: 255 }),
+  body('nome').isLength({ min: 3, max: 255 }).matches(/^[a-zA-Z0-9 .-@_]*$/).trim(),
+  body('tipo').isIn([0, 1]).isNumeric(),
+  usuarioController.criar)
+
+router.post('/api/v1/auth',
+  body('senha').isLength({ min: 8, max: 255 }),
+  body('login').isLength({ min: 5, max: 255 }).matches(/^[a-zA-Z0-9.@_]*$/).not().isEmpty().trim(),
+  autenticacaoController.autenticar)
+
+router.post('/api/v1/estabelecimento',
+  body('nome').isLength({ min: 3, max: 255 }).matches(/^[a-zA-Z0-9 .-@_]*$/),
+  body('codigo_banco').isString().isLength({ min: 3, max: 3 }),
+  body('agencia').isString().isLength({ min: 1, max: 16 }),
+  body('agencia_dv').isString().isLength({ min: 1, max: 4 }),
+  body('conta').isString().isLength({ min: 1, max: 16 }),
+  body('conta_dv').isString().isLength({ min: 1, max: 4 }),
+  body('nome_legal').isString().isLength({ min: 3, max: 255 }),
+  oneOf([
+    body('numero_documento').isString().isLength({ min: 11, max: 11 })
+      .custom(val => {
+        if (cpf.isValid(val)) {
+          return true
+        }
+
+        throw new Error('CPF invalido')
+      }),
+    body('numero_documento').isString().isLength({ min: 14, max: 14 })
+      .custom(val => {
+        if (cnpj.isValid(val)) {
+          return true
+        }
+
+        throw new Error('CPNJ invalido')
+      })
+  ]),
+  estabelecimentoController.criar)
+
+// Adicionado validacao array
+router.post('/api/v1/produto',
+  body('produtos').isArray().notEmpty(),
+  produtoController.cadastrar)
+
+router.get('/api/v1/produto',
+  query('pagina').isInt({ min: 0 }),
+  query('itens').isInt({ min: 1, max: 10 }),
+  produtoController.buscar)
+
+export { router }
