4b0993eb97a1fe6f6c287ed9b0c20a0b3754df3a
Costa <victorcosta37@gmail.com>
2021-02-28 22:39:46 -0300

    chore: add bean

---
+++ src/beans/RecebedorBean.ts
@@ -0,0 +1,9 @@
+export interface Recebedor {
+  codigo_banco: string
+  agencia: string
+  agencia_dv: string
+  conta: string
+  conta_dv: string
+  nome_legal: string
+  numero_documento: string
+}
