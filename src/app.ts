b786fe9d3df037e5327747c54a649592f78cd093
Costa <victorcosta37@gmail.com>
2021-02-28 22:38:43 -0300

    chore: add retorno 500

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata'
 import express from 'express'
+import type { Request, Response } from 'express'
 import createConnection from './db'
 import { router } from './routes'
 import { validarEnvs } from './utils/ValidarEnvs'
 import actuator, { Options } from 'express-actuator'
+import { log } from '@utils/CriarLogger'
 
 const app = express()
 
@@ -14,6 +16,14 @@ const actuatorOptions: Options = {
 app.use(express.json())
 app.use(router)
 app.use(actuator(actuatorOptions))
+
+app.use((err: Error, req: Request, res: Response) => {
+  log.error('Erro na requisicao %s', req.path, err)
+
+  res.status(500).json({
+    mensagem: 'erro interno'
+  })
+})
 
 validarEnvs()
 createConnection()
