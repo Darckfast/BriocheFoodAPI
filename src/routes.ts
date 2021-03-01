af860ee20cf41cdaf1e7b159b5c32e7d6485b8f6
Costa <victorcosta37@gmail.com>
2021-02-28 22:37:47 -0300

    chore: add validacao nas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -60,6 +60,9 @@ router.post('/api/v1/estabelecimento',
 // Adicionado validacao array
 router.post('/api/v1/produto',
   body('produtos').isArray().notEmpty(),
+  body('produtos.*.nome').isString().notEmpty(),
+  body('produtos.*.preco').isFloat({ min: 0.1 }),
+  body('produtos.*.quantidade').isInt({ min: 1 }),
   produtoController.cadastrar)
 
 router.get('/api/v1/produto',
@@ -68,6 +71,23 @@ router.get('/api/v1/produto',
   produtoController.buscar)
 
 router.post('/api/v1/pedido',
+  body('carrinho').isArray().notEmpty(),
+  body('carrinho.*.produtoId').isString().isLength({ min: 50, max: 64 }),
+  body('carrinho.*.quantidade').isInt({ min: 1 }),
+  body('pagamento').isObject().notEmpty(),
+  body('pagamento.numero_cartao').isString().isLength({ min: 16, max: 16 }).notEmpty(),
+  body('pagamento.cvv').isString().isLength({ min: 3, max: 4 }).notEmpty(),
+  body('pagamento.data_expiracao').isString().isLength({ min: 4, max: 4 }).notEmpty(),
+  body('pagamento.nome').isString().isLength({ min: 3, max: 255 }).notEmpty().matches(/^[a-zA-Z0-9 ]*$/),
+  body('envio').isObject().notEmpty(),
+  body('envio.nome').isString().isLength({ min: 3, max: 255 }).notEmpty().matches(/^[a-zA-Z0-9 ]*$/),
+  body('envio.endereco').isObject().notEmpty(),
+  body('envio.endereco.estado').isString().notEmpty().isLength({ min: 2, max: 2 }),
+  body('envio.endereco.cidade').isString().notEmpty().isLength({ min: 2, max: 255 }),
+  body('envio.endereco.bairro').isString().notEmpty().isLength({ min: 2, max: 255 }),
+  body('envio.endereco.rua').isString().notEmpty().isLength({ min: 2, max: 255 }),
+  body('envio.endereco.numero').isString().matches(/^[a-zA-Z0-9 ]*$/).notEmpty().isLength({ min: 1, max: 255 }),
+  body('envio.endereco.cep').isString().notEmpty().isLength({ min: 8, max: 8 }),
   pedidoController.criar)
 
 export { router }
